6a0bbdabdadff51f6352015e5e1f7e749ed51cef
Yaseen-Davids <yaseendavids477@gmail.com>
2024-05-08 13:23:14 +0200

    vercelenv

--- api/src/Server.ts
+++ api/src/Server.ts
@@ -29,7 +29,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.VERCEL_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "../../ui/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../../ui/dist", "index.html"));
