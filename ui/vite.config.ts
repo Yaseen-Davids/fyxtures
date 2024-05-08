71a7457089a34c905613b7051be2c8a4fe0cb588
Yaseen-Davids <yaseendavids477@gmail.com>
2024-05-08 12:53:08 +0200

    vite

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,6 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        format: "es",
+        strict: false,
+        entryFileNames: "[name].js",
+        dir: "dist/",
+      },
+    },
+  },
   server: {
     host: true,
     port: 5173,
